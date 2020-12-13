52d6884ac9265eef2723b45e7274c34d4aeb6197
ozang <ozan@pixerylabs.com>
2020-12-13 16:46:32 +0300

    update verifiers

--- cnn/util.py
+++ cnn/util.py
@@ -101,13 +101,13 @@ def is_verified(acc):
     elif model_name == models.resnet18.__name__ and acc >= 82.32:
         verified = True
 
-    elif model_name == models.vgg16.__name__ and acc >= 82.65:
+    elif model_name == models.vgg16.__name__ and acc >= 82.66:
         verified = True
 
-    elif model_name == models.vgg19.__name__ and acc >= 81.29:
+    elif model_name == models.vgg19.__name__ and acc >= 81.3:
         verified = True
 
-    elif model_name == models.densenet169.__name__ and acc >= 80:
+    elif model_name == models.densenet169.__name__ and acc >= 81.3:
         verified = True
 
     return verified
